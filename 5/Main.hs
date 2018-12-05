013149107154d394e89853ed226d50c3ba38fa87
Erik Hesselink <ehesselink@palantir.com>
2018-12-05 11:46:40 +0100

    Day 5

---
+++ 5/Main.hs
@@ -0,0 +1,33 @@
+module Main where
+
+import Data.Char
+
+main :: IO ()
+main = do
+  f <- readFile "input"
+  let polymer = map ord . init $ f -- Init to drop \n
+  putStrLn $ "Answer one: " ++ show (length . react $ polymer)
+  let polymers = [ removeUnit u polymer | u <- map ord ['A'..'Z'] ]
+      reacted = map react polymers
+  putStrLn $ "Answer two: " ++ show (minimum . map length $ reacted)
+
+type Polymer = [Unit]
+type Unit = Int
+
+react :: Polymer -> Polymer
+react s =
+  let s' = reactOnce s
+  in if s' == s
+     then s
+     else react s'
+
+reactOnce :: Polymer -> Polymer
+reactOnce (x:x':xs) | sameTypeOppositePolatiry x x' = reactOnce xs
+                    | otherwise                     = x : reactOnce (x':xs)
+reactOnce xs = xs
+
+sameTypeOppositePolatiry :: Unit -> Unit -> Bool
+sameTypeOppositePolatiry c1 c2 = abs (c1 - c2) == 32 -- toLower c1 == toLower c2 && c1 /= c2
+
+removeUnit :: Unit -> Polymer -> Polymer
+removeUnit u = filter (\u' -> u' /= u && u' /= u+32)
